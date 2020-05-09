1ac57c1c6bd81aaa7ae58a3e1180231df26a9c78
Riley Dyer <rileydyer1@gmail.com>
2020-05-09 15:02:03 -0700

    test

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,6 +1,7 @@
 // Make connection
-var socket = io.connect('http://wikibowl.herokuapp.com/');
+//var socket = io.connect('http://wikibowl.herokuapp.com/');
 //var socket = io.connect('http://localhost:3000');
+var socket = io();
 socket.on('connect', () => {
   document.getElementById('test').innerHTML = socket.id;
 });
